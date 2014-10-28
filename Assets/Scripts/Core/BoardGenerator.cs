51ea3608e586d6ac3bc0257cd4697fe9ffdbaea0
Suui <suui.fx@gmail.com>
2014-10-28 18:03:54 +0100

    Added BoardGenerator as the only stance that knows/modifies the board

---
+++ Assets/Scripts/Core/BoardGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+namespace Medusa
+{
+
+    class BoardGenerator
+    {
+
+        private Board board;
+
+        private GameObject boardCellPrefab;
+        private int boardRows, boardColumns;
+
+
+        public BoardGenerator(GameObject boardCellPrefab, int boardRows, int boardColumns)
+        {
+            this.boardCellPrefab = boardCellPrefab;
+            this.boardRows = boardRows;
+            this.boardColumns = boardColumns;
+        }
+
+
+        public void CreateBoard()
+        {
+            board = new Board(boardRows, boardColumns, "terrain", "tokens", "effects", "overlays");
+
+            foreach (Position pos in board.Positions())
+            {
+                GameObject go = Object.Instantiate(boardCellPrefab) as GameObject;
+                go.name = "cell " + pos;
+                go.transform.position = pos;
+
+                board["terrain"][pos] = go;
+            }
+        }
+
+
+        public Board Board
+        {
+            get { return board; }
+        }
+    } 
+}
