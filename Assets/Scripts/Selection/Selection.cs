3228157dbde0e0430f46e038de5588d61e2f2a1d
Suui <suui.fx@gmail.com>
2014-10-29 13:52:53 +0100

    Selection states initial structure

--- Assets/Scripts/Selection/Selection.cs
+++ Assets/Scripts/Selection/Selection.cs
@@ -5,15 +5,52 @@ namespace Medusa
 
     class Selection
     {
-        
+
+        private SelectionStates selectionState;
+
+        public Selection()
+        {
+            selectionState = SelectionStates.NothingSelected;
+        }
 
 
+        private void OnNothingSelected()
+        {
+            
+        }
+
+
+        private void OnCharacterSelected()
+        {
+            
+        }
+
+
+        private void OnSkillSeleceted()
+        {
+            
+        }
+
+
+        private void OnSkillConfirmAction()
+        {
+            
+        }
+
+
+        public SelectionStates SelectionState
+        {
+            get { return selectionState; }
+        }
     }
 
 
     public enum SelectionStates
     {
-        
+        NothingSelected,
+        CharacterSelected,
+        SkillSelected,
+        SkillConfirmAction
     }
 
 }
