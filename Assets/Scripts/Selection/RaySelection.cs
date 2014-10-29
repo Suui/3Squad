a9e398446ba63fc440e83a17654797640583fb36
Suui <suui.fx@gmail.com>
2014-10-29 22:39:28 +0100

    Added RaySelection with event trigger SelectAction.OnSelection

--- Assets/Scripts/Selection/RaySelection.cs
+++ Assets/Scripts/Selection/RaySelection.cs
@@ -7,11 +7,15 @@ namespace Medusa
     class RaySelection : MonoBehaviour
     {
 
+        public delegate void SelectAction(Position selectedPos);
+        public static event SelectAction OnSelection;
+
         private Position selectedPos;
 
         private Ray raySelection;
         private RaycastHit rayHit;
         private Layer overlay;
+
 
         void Update()
         {
@@ -22,9 +26,11 @@ namespace Medusa
                 if (Physics.Raycast(raySelection, out rayHit))
                 {
                     selectedPos = new Position((int)rayHit.transform.position.x, (int)rayHit.transform.position.z);
+
+                    if (OnSelection != null)
+                        OnSelection(selectedPos);
                 }
             }
-
         }
 
 
