27e3a4c8069a24e234d14e10aed83d6e05121a7e
Suui <suui.fx@gmail.com>
2014-11-07 22:40:35 +0100

    Added primitive version of TurnManagement

---
+++ Assets/Scripts/GameLogic/TurnManagement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+namespace Medusa
+{
+
+    class TurnManagement
+    {
+
+        private readonly GameObject masterOne;
+        private readonly GameObject masterTwo;
+        private GameObject currentMasterPlaying;
+
+
+        public TurnManagement(GameObject masterOne, GameObject masterTwo)
+        {
+            this.masterOne = masterOne;
+            this.masterTwo = masterTwo;
+        }
+
+
+        public void ChangeTurn()
+        {
+            if (currentMasterPlaying == masterOne)
+                currentMasterPlaying = masterTwo;
+            else
+                currentMasterPlaying = masterOne;
+        }
+
+
+        public GameObject CurrentMasterPlaying
+        {
+            get { return currentMasterPlaying; }
+        }
+
+
+        public GameObject EnemyMasterThisTurn
+        {
+            get
+            {
+                if (currentMasterPlaying == masterOne)
+                    return masterTwo;
+
+                return masterOne;
+            }
+        }
+
+
+        public GameObject MasterOne
+        {
+            get { return masterOne; }
+        }
+
+        public GameObject MasterTwo
+        {
+            get { return masterTwo; }
+        }
+    }
+
+}
