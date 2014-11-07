0ef4ecda5e6db7dee3c7763411e7c339c1c5d73a
Suui <suui.fx@gmail.com>
2014-11-07 22:34:00 +0100

    Added Player component

---
+++ Assets/Scripts/Components/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace Medusa
+{
+
+    class Player : MonoBehaviour
+    {
+
+        private readonly GameObject masterPlayer;
+
+
+        public Player(GameObject masterPlayer)
+        {
+            this.masterPlayer = masterPlayer;
+        }
+
+
+        public GameObject MasterPlayer
+        {
+            get { return masterPlayer; }
+        }
+    }
+
+}
