6e74c969fa03aee7a81b5263ce2a9a54407f9446
Suui <suui.fx@gmail.com>
2014-11-25 22:08:58 +0100

    Added Initialization script, seems to work

---
+++ Assets/Scripts/Initialization.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace Medusa
+{
+
+	class Initialization : MonoBehaviour
+	{
+
+		void Start()
+		{
+			GameObject playerOne = new GameObject("Player 01");
+
+			GameObject gameMaster = Instantiate(Resources.Load("Prefabs/GameMaster")) as GameObject;
+			gameMaster.name = "GameMaster";
+			gameMaster.tag = "GameMaster";
+			gameMaster.transform.parent = playerOne.transform;
+		}
+
+	}
+
+}
