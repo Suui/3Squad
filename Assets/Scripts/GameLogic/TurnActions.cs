dde5fb91dca353fa513262563fe8c02d2d4c42b3
Suui <suui.fx@gmail.com>
2014-12-13 22:28:17 +0100

    Added JSON generation and parsing but still doesn't work

--- Assets/Scripts/GameLogic/TurnActions.cs
+++ Assets/Scripts/GameLogic/TurnActions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using SimpleJSON;
 
 
 namespace Medusa
@@ -9,14 +11,46 @@ namespace Medusa
 
         public TurnActions(List<Action> actions)
         {
-	        Actions = actions;
+			JSONClass json = GenerateJSON(actions);
+	        Actions = ParseJSON(json);
         }
 
 
-        private void GenerateJSON()
+		private JSONClass GenerateJSON(List<Action> actions)
         {
-            // set up JSON...
+	        JSONClass json = new JSONClass();
+	        int c = 0, count = 0;
+
+	        foreach (var action in actions)
+	        {
+				json["actions"][c]["charpos"] = action.CharacterPos.ToJSON();
+		        json["actions"][c]["skillname"] = action.SkillName;
+
+		        foreach (var pos in action.TargetPositions)
+		        {
+					json["actions"][c]["targetpos"][count] = pos.ToJSON();
+			        count++;
+		        }
+
+		        c++;
+	        }
+
+			return json;
         }
+
+
+		private List<Action> ParseJSON(JSONClass json)
+	    {
+			var parsed = JSON.Parse(json);
+
+			return (from JSONNode action in parsed["actions"].AsArray 
+					let charPos = Position.FromJSON(action["charpos"]) 
+					let skillName = action["skillname"].Value 
+					let positions = (from JSONNode pos in action["targetpos"].AsArray 
+									 select Position.FromJSON(pos)).ToList() 
+
+					select new Action(charPos, skillName, positions)).ToList();
+	    }
 
 
         public List<Action> Actions { get; private set; }
